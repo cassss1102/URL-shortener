404b47bf7b780e17531740c40f0e06c8c65429f0
cassss1102 <zhengsuxin@gmail.com>
2017-06-14 16:21:05 -0700

    Complete phase 3

--- app/models/shortened_url.rb
+++ app/models/shortened_url.rb
@@ -1,13 +1,35 @@
+# == Schema Information
+#
+# Table name: shortened_urls
+#
+#  id         :integer          not null, primary key
+#  short_url  :string           not null
+#  long_url   :string           not null
+#  user_id    :integer          not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class ShortenedUrl < ApplicationRecord
   validates :short_url, presence: true, uniqueness: true
   validates :long_url, :user_id, presence: true
 
   belongs_to :submitter,
-  primary_key: :id,
-  foreign_key: :user_id,
-  class_name: :User
+    primary_key: :id,
+    foreign_key: :user_id,
+    class_name: :User
 
-  
+  has_many :visits,
+    primary_key: :id,
+    # foreign_key: :user_id,
+    foreign_key: :shortened_url_id,
+    class_name: :Visit
+
+  has_many :visitors,
+    -> { distinct },
+    through: :visits, # [visit1, visit2, visit3]
+    source: :visitor # [user1, user2, user1]
+
 
   def self.random_code
     code = SecureRandom.urlsafe_base64[0..15]
@@ -19,7 +41,20 @@ class ShortenedUrl < ApplicationRecord
 
   def self.shorten_url(user, long_url)
     ShortenedUrl.create!(short_url: ShortenedUrl.random_code,
-    long_url: long_url, user_id: user.id)
+      long_url: long_url, user_id: user.id)
+  end
+
+  def num_clicks
+    self.visits.count
+  end
+
+  def num_uniques
+    self.visitors.count
+    # self.visits.select(:user_id).distinct.count
+  end
+
+  def num_recent_uniques
+    self.visits.where(["created_at >= ?", 10.minutes.ago]).distinct.count
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170614185923) do
+ActiveRecord::Schema.define(version: 20170614210900) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,14 @@ ActiveRecord::Schema.define(version: 20170614185923) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
+  end
+
+  create_table "visits", force: :cascade do |t|
+    t.integer  "shortened_url_id", null: false
+    t.integer  "user_id",          null: false
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
+    t.index ["shortened_url_id", "user_id"], name: "index_visits_on_shortened_url_id_and_user_id", using: :btree
   end
 
 end
