8067c28a8cf33472c5db77bb1c0f1cb3ca8166e8
cassss1102 <zhengsuxin@gmail.com>
2017-06-14 16:34:03 -0700

    Add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Visit.destroy_all
+User.destroy_all
+ShortenedUrl.destroy_all
+
+user1 = User.new(email: "user1@gmail.com")
+user2 = User.new(email: "user2@gmail.com")
+
+user1.save
+user2.save
+
+url1 = ShortenedUrl.shorten_url(user1, "www.google.com")
+url2 = ShortenedUrl.shorten_url(user2, "www.amazon.com")
+
+url1.save
+url2.save
+
+Visit.record_visit!(user2, url1)
+Visit.record_visit!(user1, url1)
+
+Visit.record_visit!(user1, url2)
+Visit.record_visit!(user1, url2)
+Visit.record_visit!(user1, url2)
+Visit.record_visit!(user1, url2)
+Visit.record_visit!(user1, url2)
+Visit.record_visit!(user1, url2)
+Visit.record_visit!(user1, url2)
+Visit.record_visit!(user1, url2)
+Visit.record_visit!(user2, url2)
